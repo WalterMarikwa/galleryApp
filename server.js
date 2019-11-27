6a2b515f39dbad7f65868348ad78e64fe964e975
Walter Marikwa <waltermarikwa02@gmail.com>
2019-11-27 13:24:29 -0500

    Updating server

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 var express = require("express");
 var mongoose = require("mongoose");
-
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -12,10 +11,24 @@ app.use(express.json());
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/imageperformance", {
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	uri = process.env.ATLAS_URI  // connection string for Atlas here  
+  } else {
+	uri = process.env.LOCAL_URI   // connection string for localhost mongo here  
+  }
+  
+  // connection to database
+  mongoose.connect(process.env.ATLAS_URI, { 
   useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+  useUnifiedTopology: true, 
+	useFindAndModify: false,
+	useCreateIndex: true 
+  });
+  const connection = mongoose.connection;
+  connection.once('open', () => {
+	console.log("MongoDB connection is live");
+  })
 
 app.listen(PORT, function() {
   console.log(`Now listening on port: ${PORT}`);
